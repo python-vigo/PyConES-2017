9c0597d1e04423fb8ef507caf39405ffeba2c0c7
Ivan Nieto <ivan.n.s@tuta.io>
2017-03-26 17:31:06 +0200

    Uncomment for local development

--- run-local.sh
+++ run-local.sh
@@ -1,40 +1,40 @@
 #!/bin/sh
 # For Ubuntu machines
 # --------------------------------------------
-# sudo apt-get update && apt-get install -y \
-#     software-properties-common
-# sudo add-apt-repository multiverse
-# sudo apt-get update && apt-get install -y \
-#    build-essential \
-#    git \
-#    libpq-dev \
-#    python3 \
-#    python3-dev \
-#    python3-pip \
-#    nginx \
-#    libssl-dev \
-#    libjpeg-dev \
-#    jpegoptim \
-#    optipng \
-#    gettext \
-#    curl
+sudo apt-get update && apt-get install -y \
+    software-properties-common
+sudo add-apt-repository multiverse
+sudo apt-get update && apt-get install -y \
+   build-essential \
+   git \
+   libpq-dev \
+   python3 \
+   python3-dev \
+   python3-pip \
+   nginx \
+   libssl-dev \
+   libjpeg-dev \
+   jpegoptim \
+   optipng \
+   gettext \
+   curl
 
 # For CentOS machines
 # --------------------------------------------
-sudo yum update && sudo yum upgrade
-sudo yum install -y \
-    epel-release \
-    git \
-    libpqxx-devel \
-    zlib \
-    openssl-devel \
-    python-devel \
-    nginx \
-    libjpeg-turbo-devel \
-    jpegoptim \
-    optipng \
-    gettext \
-    curl
+# sudo yum update && sudo yum upgrade
+# sudo yum install -y \
+#     epel-release \
+#     git \
+#     libpqxx-devel \
+#     zlib \
+#     openssl-devel \
+#     python-devel \
+#     nginx \
+#     libjpeg-turbo-devel \
+#     jpegoptim \
+#     optipng \
+#     gettext \
+#     curl
 
 pip install -r requirements/base.txt
 pip install -r requirements/production.txt
@@ -43,10 +43,13 @@ pip install -r requirements/test.txt
 
 sed -i 's/\r//' entrypoint-local.sh \
     && chmod +x entrypoint-local.sh \
-    && chown initios entrypoint-local.sh \
+    && chown ivan entrypoint-local.sh \
     && sed -i 's/\r//' run-local.sh \
     && chmod +x run-local.sh \
-    && chown initios run-local.sh
+    && chown ivan run-local.sh
+    # && chown initios entrypoint-local.sh \
+    # && chown initios run-local.sh
+
 
 npm install \
 && npm run build \
