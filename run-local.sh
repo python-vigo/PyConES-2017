2dc3177612f9a435ba725c1de80ebb9f590e0f13
Ivan Nieto <ivan.n.s@tuta.io>
2017-03-26 22:26:24 +0200

    Mod local script

--- run-local.sh
+++ run-local.sh
@@ -1,60 +1,60 @@
-#!/bin/sh
-# For Ubuntu machines
-# --------------------------------------------
-sudo apt-get update && apt-get install -y \
-    software-properties-common
-sudo add-apt-repository multiverse
-sudo apt-get update && apt-get install -y \
-   build-essential \
-   git \
-   libpq-dev \
-   python3 \
-   python3-dev \
-   python3-pip \
-   nginx \
-   libssl-dev \
-   libjpeg-dev \
-   jpegoptim \
-   optipng \
-   gettext \
-   curl
+# #!/bin/sh
+# # For Ubuntu machines
+# # --------------------------------------------
+# sudo apt-get update && apt-get install -y \
+#     software-properties-common
+# sudo add-apt-repository multiverse
+# sudo apt-get update && apt-get install -y \
+#    build-essential \
+#    git \
+#    libpq-dev \
+#    python3 \
+#    python3-dev \
+#    python3-pip \
+#    nginx \
+#    libssl-dev \
+#    libjpeg-dev \
+#    jpegoptim \
+#    optipng \
+#    gettext \
+#    curl
 
-# For CentOS machines
-# --------------------------------------------
-# sudo yum update && sudo yum upgrade
-# sudo yum install -y \
-#     epel-release \
-#     git \
-#     libpqxx-devel \
-#     zlib \
-#     openssl-devel \
-#     python-devel \
-#     nginx \
-#     libjpeg-turbo-devel \
-#     jpegoptim \
-#     optipng \
-#     gettext \
-#     curl
+# # For CentOS machines
+# # --------------------------------------------
+# # sudo yum update && sudo yum upgrade
+# # sudo yum install -y \
+# #     epel-release \
+# #     git \
+# #     libpqxx-devel \
+# #     zlib \
+# #     openssl-devel \
+# #     python-devel \
+# #     nginx \
+# #     libjpeg-turbo-devel \
+# #     jpegoptim \
+# #     optipng \
+# #     gettext \
+# #     curl
 
-pip install -r requirements/base.txt
-pip install -r requirements/production.txt
-pip install -r requirements/local.txt
-pip install -r requirements/test.txt
+# pip install -r requirements/base.txt
+# pip install -r requirements/production.txt
+# pip install -r requirements/local.txt
+# pip install -r requirements/test.txt
 
-sed -i 's/\r//' entrypoint-local.sh \
-    && chmod +x entrypoint-local.sh \
-    && chown ivan entrypoint-local.sh \
-    && sed -i 's/\r//' run-local.sh \
-    && chmod +x run-local.sh \
-    && chown ivan run-local.sh
-    # && chown initios entrypoint-local.sh \
-    # && chown initios run-local.sh
+# sed -i 's/\r//' entrypoint-local.sh \
+#     && chmod +x entrypoint-local.sh \
+#     && chown ivan entrypoint-local.sh \
+#     && sed -i 's/\r//' run-local.sh \
+#     && chmod +x run-local.sh \
+#     && chown ivan run-local.sh
+#     # && chown initios entrypoint-local.sh \
+#     # && chown initios run-local.sh
 
 
 npm install \
 && npm run build \
 && python3 manage.py collectstatic --noinput \
-&& python3 manage.py compilemessages \
 && python3 manage.py migrate \
 && python3 manage.py runserver
 
+# && python3 manage.py compilemessages \
