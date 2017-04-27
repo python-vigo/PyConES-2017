086d316eced64fcb744e49d78194202173ba97a8
Ivan Nieto <ivan.n.s@tuta.io>
2017-04-27 18:52:51 +0200

    Update locale GL

--- dev-locales.sh
+++ dev-locales.sh
@@ -2,6 +2,4 @@
 
 python3 manage.py collectstatic --noinput \
 && python3 manage.py compilemessages \
-&& python3 manage.py makemigrations \
-&& python3 manage.py migrate \
-&& python3 manage.py runserver
+
